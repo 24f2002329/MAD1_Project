584d63869dee0483d937789d961c21fcf1bd125d
devendra-sjt <devendrachouhan.sjt@gmail.com>
2025-01-05 16:17:05 +0530

    Created Registration And Login Pages

--- QuizMaster/app/routes/auth.py
+++ QuizMaster/app/routes/auth.py
@@ -8,3 +8,16 @@ def home():
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+@auth.route('/scores')
+def scores():
+    return render_template('scores.html')
+
+@auth.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('user.home'))
+
+@auth.route('/summary')
+def summary():
+    return render_template('summary.html')
--- QuizMaster/app/routes/user.py
+++ QuizMaster/app/routes/user.py
@@ -9,3 +9,32 @@ def home():
 @user.route('/login')
 def login():
     return render_template('login.html')
+
+@user.route('/scores')
+def scores():
+    return render_template('scores.html')
+
+@user.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('user.home'))
+
+@user.route('/summary')
+def summary():
+    return render_template('summary.html')
+
+@user.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+@user.route('/forgot_username_password')
+def forgot_username_password():
+    return render_template('forgot_username_password.html')
+
+@user.route('/admin_login')
+def admin_login():
+    return render_template('admin_login.html')
+
+@user.route('/admin_home')
+def admin_home():
+    return render_template('admin_home.html')
